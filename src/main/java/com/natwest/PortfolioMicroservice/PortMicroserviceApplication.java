3f45f6537099cd77de3abd538be73e5652a89449
Ubuntu <ubuntu@ip-172-31-8-5.ap-south-1.compute.internal>
2021-08-26 07:32:39 +0000

    add portfolio microservice

---
+++ src/main/java/com/natwest/PortfolioMicroservice/PortMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.natwest.PortfolioMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/natwest/PortMicroservice/PortMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.natwest.PortMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
